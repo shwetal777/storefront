ce8677be20534a227b37856eccb4d533c45c4c6c
Shwetal More <more.shwetal777@gmail.com>
2021-10-16 12:34:52 +0530

    update

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,11 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Shwetal'})
