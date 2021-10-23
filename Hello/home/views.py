6c29c52cbe76a1008f09e956adec4bcbf75f56c8
Shwetal More <more.shwetal777@gmail.com>
2021-10-23 16:33:52 +0530

    newup

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     context = {
@@ -18,5 +20,15 @@ def services(request):
     # return HttpResponse('This is services page')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent.')
     return render(request, 'contact.html')
     # return HttpResponse('This is contact  page')
+
+    
